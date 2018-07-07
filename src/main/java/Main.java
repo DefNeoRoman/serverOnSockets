5210945e73b96c6de40bf1377c110e36eb1f9504
rgaponov <rgaponov@omnicomm.ru>
2018-07-07 21:58:40 +0300

    add tester

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,16 @@
-public class Main {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args){
+         try
+        {
+            ChatServer server = new ChatServer(5050);
+
+            server.start();
+            System.out.println("Server started");
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
