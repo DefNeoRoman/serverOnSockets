8e86c6122c5e5e401cedb3865e70d142e161d473
rgaponov <rgaponov@omnicomm.ru>
2018-07-08 11:00:08 +0300

    refactor

--- src/main/java/versionOne/ChatServer.java
+++ src/main/java/versionOne/ChatServer.java
@@ -6,20 +6,23 @@ import java.net.ServerSocket;
 public class ChatServer {
     private ServerSocket serverSocket;
     private boolean finished;
+
     public ChatServer(int port) throws IOException {
         serverSocket = new ServerSocket(port);
         serverSocket.setSoTimeout(100000);
         finished = false;
     }
-    public void close(){
+
+    public void close() {
         finished = true;
     }
-    public void start(){
-        while(!finished){
+
+    public void start() {
+        while (!finished) {
             try {
                 System.out.println("Server started");
-                new ChatSocket(serverSocket.accept(),this).start();
-            }catch (IOException io){
+                new ChatSocket(serverSocket.accept(), this).start();
+            } catch (IOException io) {
                 io.printStackTrace();
             }
         }
--- src/main/java/versionOne/ChatSocket.java
+++ src/main/java/versionOne/ChatSocket.java
@@ -8,28 +8,29 @@ import java.net.Socket;
 public class ChatSocket extends Thread {
     Socket socket;
     ChatServer chatServer;
-    public ChatSocket(Socket socket, ChatServer chatServer){
+
+    public ChatSocket(Socket socket, ChatServer chatServer) {
         this.socket = socket;
         this.chatServer = chatServer;
     }
+
     @Override
-    public void run(){
-        try{
+    public void run() {
+        try {
             DataInputStream in =
                     new DataInputStream(socket.getInputStream());
             String message = in.readUTF();
             System.out.println(message);
-            if(!message.equals("Bue.")) {
+            if (!message.equals("Bue.")) {
                 DataOutputStream out =
                         new DataOutputStream(socket.getOutputStream());
                 out.writeUTF(message);
                 out.flush();
 
-            }
-            else chatServer.close();
+            } else chatServer.close();
             in.close();
             socket.close();
-        }catch (IOException io){
+        } catch (IOException io) {
             io.printStackTrace();
         }
     }
